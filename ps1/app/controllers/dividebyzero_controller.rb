413f0c55f3ea0c5853f6eefdba508940f5bdd36c
st116575 <st116575@ait.asia>
2014-09-08 15:49:32 +0700

    I have added divide by zero controller and updated some files including show, index in view folder

---
+++ ps1/app/controllers/dividebyzero_controller.rb
@@ -0,0 +1,27 @@
+class DividebyzeroController < ApplicationController
+
+# GET /divideyzero
+# GET /divideyzero.json 
+  def index
+   
+  end
+
+ def show
+   if params[:second]
+   @data1 = params[:first]
+   @data2 = params[:second]
+   begin
+     logger.info "About Divide by zero exceptions"
+     @data3 = @data1.to_i / @data2.to_i
+   rescue => e
+     @data3 = 0
+     #render :text => "About Divide by zero exception"
+     #render :text => e.message
+     render :text => "<h1>"+ e.message + "</h1>" +e.backtrace.join("\n")
+   end
+   #parmas[:result] = @data3
+   end
+ end
+   #redirect_to :action=>"index" #show the home page itself
+ #redirect_to :action=>"dividebyzero#index" #show the home page itself
+end
