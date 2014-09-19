c1ef327ce0e0a2d60e01931dd85d7641c93d25bb
Peeranat Sangkatumvong <rozone_2k@hotmail.com>
2014-09-19 14:14:03 +0700

    change the way to route , fixed search submit path

--- ps2/config/deploy.rb
+++ ps2/config/deploy.rb
@@ -40,7 +40,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, File.join(release_path,fetch(:application),'/tmp/restart.txt')
     end
   end
 
