ae90bded2683ebf7e80c9a949f80162e8dabe6b1
st116575 <st116575@ait.asia>
2014-09-26 10:28:39 +0700

    Work done on first five point and will be furnish later on as well...

---
+++ ps3/app/controllers/projectpalan_controller.rb
@@ -0,0 +1,7 @@
+class ProjectpalanController < ApplicationController
+  def new
+  end
+  def index
+
+  end
+end
---
+++ ps3/test/helpers/projectpalan_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class ProjectpalanHelperTest < ActionView::TestCase
+end
