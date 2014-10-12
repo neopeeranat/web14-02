590a498c0b29436ba79042d8e8e51b157b5d0ccf
st116575 <st116575@ait.asia>
2014-10-12 14:19:25 +0700

    these are the additional changes for pS4

--- ps4/app/controllers/user_menagement_controller.rb
+++ ps4/app/controllers/user_menagement_controller.rb
@@ -1,10 +1,13 @@
 class UserMenagementController < ApplicationController
 
-  before_filter :authenticate_user!, :except => [:index, :show]
-  before_action :login_required, except: [:index, :show]
-  before_action :role_required, except: [:index, :show]
+  before_filter :authenticate_user!, :except => [:index]
+  before_action :login_required, except: [:index]
+  before_action :role_required, except: [:index]
 
   def index
 
+  end
+  def show
+    render :text =>  'Show method is not mentions in view part of Rails'
   end
 end
