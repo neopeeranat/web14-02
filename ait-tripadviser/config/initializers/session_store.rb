0c7cf6021402532a8b2c9ff9fff9c2cfee8249db
Peeranat Sangkatumvong <rozone_2k@hotmail.com>
2014-10-19 05:01:31 +0700

    create home, service interface modified navbar login and color tab

--- ait-tripadviser/config/initializers/session_store.rb
+++ ait-tripadviser/config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ait_tripadviser_session'
+Rails.application.config.session_store :cookie_store, key: '_ait_tripadvisor_session'
--- ait-tripadviser/config/routes.rb
+++ ait-tripadviser/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'service/' => 'service#index'
+
   ActiveAdmin.routes(self)
   root 'home#index'
-
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
