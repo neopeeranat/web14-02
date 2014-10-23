0fdc272934ff226a27d60253574b4733f5d03e3c
st116575 <st116575@ait.asia>
2014-10-24 00:34:47 +0700

    Pai and line graph have been added to the dashboard.......

--- ait-tripadvisor/app/admin/dashboard.rb
+++ ait-tripadvisor/app/admin/dashboard.rb
@@ -4,10 +4,10 @@ ActiveAdmin.register_page "Dashboard" do
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+      #span class: "blank_slate" do
+        #span I18n.t("active_admin.dashboard_welcome.welcome")
+       # small I18n.t("active_admin.dashboard_welcome.call_to_action")
+      #end
     end
   section "User Statistics" do
     table_for User.all do
@@ -17,14 +17,17 @@ ActiveAdmin.register_page "Dashboard" do
       column :updated_at
       #actions
     end
-    section "Graph for Users" do
-      div do
-        render "graph"
-        end
-        #var r = Raphael(10, 50, 640, 480);
-        #r.piechart(320, 240, 100, [55, 20, 13, 32, 5, 1, 2]);
-      end
   end
+   # section "Pai Graph" do
+      #div do
+        #render "pai_graph"
+      #end
+    #end
+    section "Line Graph" do
+      div do
+        render "line_graph"
+      end
+    end
      # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
