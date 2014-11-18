d602d0ff926ba7d5a15b6f18f4d4b5b1f3d52995
Peeranat Sangkatumvong <rozone_2k@hotmail.com>
2014-11-19 03:46:19 +0700

    fix social login(facebook) to able to use localhost for test and production.   add some more detail on direction details , can view other user profile.

--- ait-tripadvisor/config/routes.rb
+++ ait-tripadvisor/config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
 
   #User Management
   get 'users' => 'users#index'
-  resources :user, only: [:show]
   get 'users/directions'=> 'users#directions'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" ,:registrations => "users/registrations"}, :skip => [:sessions]
   devise_scope :user do
@@ -18,7 +17,9 @@ Rails.application.routes.draw do
   namespace :users do
     resources :directions, only: [:show, :edit, :update, :destroy]
   end
+  resources :users, only: [:show]
 
+  
   #Service
   get 'service/' => 'service#index'
   namespace :service do
